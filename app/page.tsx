351dc384aadc396518553a3d4b1363e6510e60ec
Alamin Shaikh <hello@octib.com>
2025-03-28 10:09:45 +0600

    Updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,47 @@
-export default function Home() {
-  return <main>Homepage</main>;
+import Header from '@/components/home/Header';
+import { openGraph } from '@/lib/metadata';
+import { Metadata } from 'next';
+import AboutMe from '@/components/home/AboutMe';
+import CTA from '@/components/home/CTA';
+import FAQ from '@/components/home/FAQ';
+import Features from '@/components/home/Features';
+import Footer from '@/components/home/Footer';
+import Hero from '@/components/home/Hero';
+import Promotion from '@/components/home/Promotion';
+import ProblemStatement from '@/components/home/ProblemStatement';
+import Pricing from '@/components/home/Pricing';
+import Problems from '@/components/home/Problems';
+import Comparison from '@/components/home/Comparison';
+
+export default function HomePage() {
+  return (
+    <>
+      <Promotion />
+      <Header />
+      <main>
+        <Hero />
+        <ProblemStatement />
+        <Problems />
+        <Features />
+        <Comparison />
+        <AboutMe />
+        <Pricing />
+        <FAQ />
+        <CTA />
+      </main>
+      <Footer />
+    </>
+  );
 }
+
+export const metadata: Metadata = {
+  title: 'UPDATE: Title | Business',
+  description: 'UPDATE: Description',
+  openGraph: {
+    ...openGraph,
+    title: 'UPDATE: Title',
+  },
+  alternates: {
+    canonical: '/',
+  },
+};
