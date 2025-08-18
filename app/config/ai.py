d5da5ce41c343d51926c781f4e6c7d0ae8340ffc
Alamin Shaikh <hello@octib.com>
2025-08-18 11:00:17 +0600

    fix: update ai config and use lite model for fin-sight agent

--- app/config/ai.py
+++ app/config/ai.py
@@ -6,27 +6,37 @@ from app.config.env import MISTRAL_API_KEY, IS_DEV, GEMINI_API_KEY
 gemini_base_url = "https://generativelanguage.googleapis.com/v1beta/openai"
 mistral_base_url = "https://api.mistral.ai/v1"
 
-gemini_model = "gemini-2.5-flash"
+gemini_pro_model = "gemini-2.5-pro"
+gemini_lite_model = "gemini-2.5-flash"
 mistral_model = "mistral-medium-latest"
 
 
-def get_openai_model(use_premium_model: bool = False) -> OpenAIChatCompletionsModel:
-    if IS_DEV or not use_premium_model:
+def get_openai_model(
+    use_lite_model: bool = False, use_pro_model: bool = False
+) -> OpenAIChatCompletionsModel:
+    if not IS_DEV and use_lite_model:
         return OpenAIChatCompletionsModel(
-            model=mistral_model,
-            openai_client=AsyncOpenAI(
-                base_url=mistral_base_url, api_key=MISTRAL_API_KEY
-            ),
+            model=gemini_lite_model,
+            openai_client=AsyncOpenAI(base_url=gemini_base_url, api_key=GEMINI_API_KEY),
+        )
+
+    if not IS_DEV and use_pro_model:
+        return OpenAIChatCompletionsModel(
+            model=gemini_pro_model,
+            openai_client=AsyncOpenAI(base_url=gemini_base_url, api_key=GEMINI_API_KEY),
         )
 
     return OpenAIChatCompletionsModel(
-        model=gemini_model,
-        openai_client=AsyncOpenAI(base_url=gemini_base_url, api_key=GEMINI_API_KEY),
+        model=mistral_model,
+        openai_client=AsyncOpenAI(base_url=mistral_base_url, api_key=MISTRAL_API_KEY),
     )
 
 
-def get_crewai_llm(use_premium_model: bool = False) -> LLM:
-    if IS_DEV or not use_premium_model:
-        return LLM(model=f"mistral/{mistral_model}", api_key=MISTRAL_API_KEY)
+def get_crewai_llm(use_lite_model: bool = False, use_pro_model: bool = False) -> LLM:
+    if not IS_DEV and use_lite_model:
+        return LLM(model=f"gemini/{gemini_lite_model}", api_key=GEMINI_API_KEY)
 
-    return LLM(model=f"gemini/{gemini_model}", api_key=GEMINI_API_KEY)
+    if not IS_DEV and use_pro_model:
+        return LLM(model=f"gemini/{gemini_pro_model}", api_key=GEMINI_API_KEY)
+
+    return LLM(model=f"mistral/{mistral_model}", api_key=MISTRAL_API_KEY)
